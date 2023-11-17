eda8f6f82ce81cc5f08dd2c2df3b9825b343b7fb
jovenloba <117356497+jovenloba@users.noreply.github.com>
2023-11-17 13:28:43 +0800

    change chapter 2 aDD SELF.FINE

---
+++ chapter2.py
@@ -0,0 +1,13 @@
+class test():
+    def __init__(self, sqlParam, ):
+        self.sqlParam = sqlParam
+        self.host = sqlParam[0]
+        self.database = sqlParam[1]
+        self.user = sqlParam[2]
+        self.password = sqlParam[3]
+        self.fine = True
+
+
+if __name__ == "__main__":
+    import sys
+    sqlParam = ('localhost', 'jovenschema', 'root', 'Acoje1985**')
